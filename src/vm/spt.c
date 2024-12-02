4eb12c14377f10ea107e2c1a0ee959dd21b61dee
Doyoung Kim <d0319@postech.ac.kr>
2024-12-02 21:11:34 +0900

    Fixed wrong types and compilation error.

---
+++ src/vm/spt.c
@@ -0,0 +1,69 @@
+#include "vm/spt.h"
+#include "threads/malloc.h"
+#include "threads/thread.h"
+#include <lib/user/syscall.h>
+#include <hash.h>
+
+/* Makes basic SPTE which is from UADDR, refer to a disk sector INDEX, with 
+   size of SIZE. */
+struct spte *
+spt_make_entry (void *uaddr, int size, block_sector_t index)
+{
+  struct spte *new = (struct spte *) malloc(sizeof (struct spte));
+
+  new->size = size;
+  new->swapped = true;
+  new->uaddr = uaddr;
+  new->mapid = MAP_FAILED;
+  new->index = index;
+
+  return new;
+}
+
+/* Frees allocated space for SPTE. */
+void
+spt_free_entry (struct spte *spte)
+{
+  free (spte);
+}
+
+/* Finds SPTE with user address UADDR, under the current process's context. 
+   Returns found SPTE if such user address exists on the SPTE of current 
+   process. Returns null pointer if such user address does not exist. */
+struct spte *
+spt_lookup (void *uaddr)
+{
+  struct process *this = thread_current ()->process;
+
+  ASSERT (this != NULL);
+
+  struct spte entry;
+  struct hash_elem *e;
+
+  entry.uaddr = uaddr;
+  e = hash_find (&this->spt, &entry.elem);
+
+  return e ? hash_entry (e, struct spte, elem) : NULL;
+}
+
+/* Hash function to be used for SPT, which essensially computes hash for the
+   UADDR for the SPTE. */
+unsigned
+spt_hash (const struct hash_elem *e, void *aux UNUSED)
+{
+  const struct spte *entry = hash_entry (e, struct spte, elem);
+  int uaddr = (int) entry->uaddr;
+
+  return hash_int (uaddr);
+}
+
+/* Less function for SPT. Returns true if A's address precedes B's. */
+bool
+spt_less_func (const struct hash_elem *a_, const struct hash_elem *b_, 
+               void *aux UNUSED)
+{
+  const struct spte *a = hash_entry (a_, struct spte, elem);
+  const struct spte *b = hash_entry (b_, struct spte, elem);
+
+  return a->uaddr < b->uaddr;
+}
---
+++ src/vm/spt.h
@@ -0,0 +1,25 @@
+#include <hash.h>
+#include <lib/user/syscall.h>
+#include <lib/stdbool.h>
+#include "devices/block.h"
+
+/* Supplemental page table element. */
+struct spte 
+  {
+    int size;
+    bool swapped;
+    void *uaddr;
+    mapid_t mapid;
+    block_sector_t index;
+    struct hash_elem elem;
+  };
+
+/* Basic operations on SPT. */
+struct spte *spt_make_entry (void *, int, block_sector_t);
+void spt_free_entry (struct spte *);
+struct spte *spt_lookup (void *);
+
+/* Auxiliary functions to be used for hash table. */
+unsigned spt_hash (const struct hash_elem *, void *);
+bool spt_less_func (const struct hash_elem *, const struct hash_elem *, 
+                    void *);
